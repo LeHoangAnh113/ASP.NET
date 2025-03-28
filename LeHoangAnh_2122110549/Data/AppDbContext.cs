5f10bae094d31c546428283df10a7416f0f75d2b
LeVanHieu15042004 <lehieupro1542004@gmail.com>
2025-03-28 14:17:38 +0700

    asp.net

---
+++ LeHoangAnh_2122110549/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿
+using LeHoangAnh_2122110549.Model;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace LeHoangAnh_2122110549.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ LeHoangAnh_2122110549/Model/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace LeHoangAnh_2122110549.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public double Price { get; set; }
+    }
+}
